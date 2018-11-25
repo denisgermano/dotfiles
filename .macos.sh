de48e36e3cf7394c7dd76d79e730b5ecb6095970
Denis Germano <dlcgermano@gmail.com>
2018-11-25 21:04:54 -0200

    Separeted file for defaults and apps

--- .macos.sh
+++ .macos.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-COMPUTERNAME="Denis' Mac2"
+# Sane OSX defaults
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until everything has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+COMPUTERNAME="Denis' Mac"
 LOCALHOSTNAME="denis-mac"
 
 # Close any open System Preferences panes, to prevent them from overriding
--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until everything has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Sane OSX defaults
-./.macos.sh
-
 # Requirements for some formula into homebrew
 xcode-select --install
 
